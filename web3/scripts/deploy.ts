6cea90c750cd1404207edfc9fb60ef2baac76960
Rohan Sharma <onlyrohan21@gmail.com>
2023-04-30 13:14:30 +0530

    BUGS

--- web3/scripts/deploy.ts
+++ web3/scripts/deploy.ts
@@ -14,9 +14,9 @@ async function main() {
   
   console.log(`Deploying a smart contract...`);
 
-  const AVAXGods = (await deploy('AVAXGods', _metadataUri)).connect(admin);
+  const DAPPGods = (await deploy('AVAXGods', _metadataUri)).connect(admin);
 
-  console.log({ AVAXGods: AVAXGods.address });
+  console.log({ DappGods: DAPPGods.address });
 }
 
 main()
